a439c504f056c08ca534369f893db3038699512d
Allozo <allozo@yandex.ru>
2021-07-24 22:53:56 +0300

    Часть 1. Дз по автоматизация тестирования апи.

--- tests/del_cat_test.ts
+++ tests/del_cat_test.ts
@@ -9,7 +9,7 @@ describe('Функционал удаления случайного кота', 
 
   it('Удаление существующего кота', async () => {
     const all_cats = await CoreApi.getAllCats();
-    console.info('Выполнили запрос GET /getAllCats')
+    console.info('Выполнили запрос GET /getAllCats');
 
     const length_group = all_cats.data.groups.length;
     const number_group = getRandomInt(length_group);
@@ -29,19 +29,42 @@ describe('Функционал удаления случайного кота', 
         console.info(`Получен ответ на запрос DELETE /cats/{catId}/remove:\n`, data);
         allure.attachment('attachment', data, 'application/json');
         return remove_response;
-      }
+      },
     );
 
     await allure.step(
       'Выполнена проверка соответствия значения id кота из ответа с переданным нами в запрос',
       () => {
-        console.info('Проверим, что в ответе будет тот же id, что и в запросе')
+        console.info('Проверим, что в ответе будет тот же id, что и в запросе');
         assert.equal(response.data.id, cat_id, 'Id не соответствуют');
 
-        console.info(`Id котика в запросе: ${cat_id} == Id котика в ответе ${response.data.id}`)
-      }
+        console.info(`Id котика в запросе: ${cat_id} == Id котика в ответе ${response.data.id}`);
+      },
     );
 
-  })
+  });
 
-})
+  it('Проверка, что котик удалился', async () => {
+    const response = await allure.step(
+      `Выполнен запрос GET /get-by-id c параметром ${cat_id}`,
+      async () => {
+        console.info('Выполняется запрос GET /get-by-id');
+        const response = await CoreApi.getCatById(cat_id);
+        const data = JSON.stringify(response.data, null, 2);
+        console.info('Получен ответ на запрос GET /get-by-id:\n', data);
+        allure.attachment('attachment', data, 'application/json');
+        return response;
+      },
+    );
+
+    await allure.step(
+      'Выполнена проверка кода ответа',
+      () => {
+        console.info('Проверим, что код ответа равен 404.')
+        assert.equal(response.status, 404, 'Статус код не тот, который ожидали');
+        console.info('Код ответа совпал.')
+      },
+    );
+  });
+
+});
